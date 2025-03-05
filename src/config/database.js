cee9ded34084db99a47afd1ac063125abeee2f5a
Letícia Schiavon <leticiaschiavon20@gmail.com>
2025-03-05 16:09:29 -0400

    feat: service, controller and routers users

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,11 @@
-import sqlite from 'sqlite';
+import sqlite3 from 'sqlite3';
 
-const db = new sqlite3.Database('library_db.sqlite', () => {
+const db = new sqlite3.Database('library_db.sqlite', (err) => {
     if (err) {
         console.error('Erro ao conectar ao banco de dados:', err.message);
     } else {
-        console.log('Conectado com sucesso ao banco de dados SQLite.');
+        console.log('Conectado ao banco de dados SQLite.');
     }
-})
+});
 
 export default db;
